81a4bd8d3687e7ee8070eabee0d5632208082fbf
Carl Barbee <carlb15@vt.edu>
2014-04-15 22:33:52 -0400

    gitignore not tracking project files

--- PIC_Framework_3.X/build/UseXC8/production/_ext/1360937237/motor_commands.p1.d
+++
@@ -1,7 +0,0 @@
- build/UseXC8/production/_ext/1360937237/motor_commands.d  \
- build/UseXC8/production/_ext/1360937237/motor_commands.p1:  \
- ../src/motor_commands.c  \
-../src/motor_commands.h  \
-../src/debug.h  \
-../src/maindefs.h  \
-../src/messages.h 
